692ce17c73e8a0faf14b871d2b0492cde0c6d1fb
seema777 <navaghareseema3@gmail.com>
2020-08-06 22:44:34 +1300

    Emp Job Type

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -24,4 +24,20 @@ if [ $isPersent -eq $randomcheck ]
        salary=0;
 fi
 
+#!/bin/bash -x
+isPartTime=1;
+isFullTime=2;
+ empRatePerHr=20;
+randomcheck=$(( $RANDOM%3 ));
+if [ $isPartTime -eq $randomcheck ]
+   then
+       empHrs=4;
+  elif [ $isFullTime -eq $randomcheck]
+      then
+       empHrs=8;
+  else
+       empHrs=0;
+fi
+       salary=$(( $empRatePerHr*$empHrs ));
+
 
