54fc90867d299e31e20147c1b96024602866b4cc
seema777 <navaghareseema3@gmail.com>
2020-08-06 22:26:10 +1300

    Employee Attendance

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
 echo "Welcome to Employee Wage Computation Program On Master Branch"
+
+#!/bin/bash -x
+isPersent=1
+randomcheck=$(( $RANDOM%2 ))
+if [[ $isPersent -eq $randomcheck ]]
+then
+  echo "Employee is Persent"
+else
+  echo "Employee is Absent"
+fi
