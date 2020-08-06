4b41c1736024025d47badf7d79dd2273892adeeb
seema777 <navaghareseema3@gmail.com>
2020-08-06 23:07:04 +1300

    EmpJobType Using SwitchCase

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -41,3 +41,21 @@ fi
        salary=$(( $empRatePerHr*$empHrs ));
 
 
+#!/bin/bash -x
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+randomcheck=$(( $RANDOM%3 ));
+case $randomcheck in
+        $isPartTime)
+        empHrs=4;
+           ;;
+        $isFullTime)
+        empHrs=8;
+           ;;
+        *)
+        empHrs=0;
+           ;;
+esac
+       salary=$(( $empRatePerHr*$empHrs ));
+
