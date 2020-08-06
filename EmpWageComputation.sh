aafba799b4664b58136bcb9115cf21ae763cfa87
seema777 <navaghareseema3@gmail.com>
2020-08-06 22:37:55 +1300

    Emp Wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -11,3 +11,17 @@ then
 else
   echo "Employee is Absent"
 fi
+
+#!/bin/bash -x
+isPersent=1;
+randomcheck=$(( $RANDOM%2 ));
+if [ $isPersent -eq $randomcheck ]
+   then
+       empRatePerHr=20;
+       empHrs=8;
+       salary=$(( $empRatePerHr*$empHrs ));
+  else
+       salary=0;
+fi
+
+
