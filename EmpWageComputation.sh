a0791bed7ab1aaea1096ba5be7ee335f426e1e2d
seema777 <navaghareseema3@gmail.com>
2020-08-06 23:31:11 +1300

    EmpWage using Function

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -114,3 +114,42 @@ esac
 totalEmpHrs=$(( $totalEmpHrs + $empHrs ));
 done
 
+
+#!/bin/bash -x
+
+IS_PART_TIME=1;
+IS_PART_TIME=2
+MAX_HRS_IN_MONTH=10;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+
+totalEmpHrs=0;
+totalWorkingDays=0;
+
+function getWorkHrs(){
+local empCheck=$1
+case $empCheck in
+  $IS_FULL_TIME)
+      empHrs=8
+         ;;
+  $IS_PART_TIME)
+       empHrs=4
+   ;;
+  *)
+    empHrs=0
+        ;;
+esac
+echo $empHrs
+}
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+((totalWorkingDays++))
+empCheck=$(($RANDOM%3));
+empHrs=$( getWorkHrs $empCheck )
+totalEmpHrs=$(( $totalEmpHrs + $empHrs ));
+dailyWage[$totalWorkingDays]=$(( $empHrs*$EMP_RATE_PER_HR ))
+((count++));
+done
+echo $count;
+totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
+
