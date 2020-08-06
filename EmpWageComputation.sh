a54a3b209bf9ea2e0a7265bc21c682b652c524fe
seema777 <navaghareseema3@gmail.com>
2020-08-06 23:25:07 +1300

    EmpWage With TotalWorkingHrs

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -84,3 +84,33 @@ esac
         totalsalary=$(( $totalsalary+$salary ))
 done
 
+
+#!/bin/bash -x
+
+IS_PART_TIME=1;
+IS_PART_TIME=2
+MAX_HRS_IN_MONTH=10;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+
+totalEmpHrs=0;
+totalWorkingDays=0;
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $TOTAL_WORKINGDAYS -lt $NUM_WORKING_DAYS ]]
+do
+((totalworkingDays++))
+empcheck=$(($RANDOM%3));
+case $empcheck in
+  $IS_FULL_TIME)
+   empHrs=8
+    ;;
+  $IS_PART_TIME)
+   empHrs=4
+    ;;
+   *)
+   empHrs=0;
+    ;;
+esac
+totalEmpHrs=$(( $totalEmpHrs + $empHrs ));
+done
+
